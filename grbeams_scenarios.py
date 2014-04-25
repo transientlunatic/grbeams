a978c42263e001ebcab0b91f40509796a2cef466
James Clark <james.clark@ligo.org>
2014-04-25 15:29:50 -0400

    scenarios now give jet posteriors for delta function efficiency priors

--- grbeams_scenarios.py
+++ grbeams_scenarios.py
@@ -37,25 +37,31 @@ __author__ = "James Clark <james.clark@ligo.org>"
 #########################
 # -*- ADE  -*- #
 
-epochs = [2015, 2016, 2017, 2019, 2022]
-predictions = ['low', 're', 'high']
-linestyles=[':','-','--']
+#epochs = [2015, 2016, 2017, 2019, 2022]
+epochs = [2016, 2022]
+#predictions = ['low', 're', 'high']
+predictions = ['re', 'high']
+linestyles=['-','--',':','.-','-']
+markers=['.','^','+','v','*']
 
-for e,epoch in enumerate(epochs):
-    print '----'
+for p,prediction in enumerate(predictions):
+#for e,epoch in enumerate(epochs):
+    print p,len(predictions)
+
     f,ax_bns_rate=pl.subplots( figsize=(8,6) )
     f,ax_jet_angle=pl.subplots( figsize=(8,6) )
 
     biggest_ul_rate=0.0
     biggest_ul_jet=0.0
-    for p,prediction in enumerate(predictions):
+    #for p,prediction in enumerate(predictions):
+    for e,epoch in enumerate(epochs):
 
         # --- Create Observation Scenario
         scenario = grbeams_utils.Scenarios(epoch=epoch, rate_prediction=prediction)
         scenario.compute_posteriors()
 
         # Only continue evaluation if Ndetections>=1
-        if scenario.Ngws < 1: continue
+        #if scenario.Ngws < 1: continue
 
         # --- Construct Jet Posteriors
         jetpos = grbeams_utils.JetPosterior(scenario,'delta,1.0')
@@ -69,10 +75,11 @@ for e,epoch in enumerate(epochs):
 
         if ul_rate>biggest_ul_rate: biggest_ul_rate = ul_rate
 
-        label_str='$R_{\mathrm{%s}}$'%prediction
+        #label_str='$R_{\mathrm{%s}}$'%prediction
+        label_str='Epoch=%s'%str(epoch)
 
         ax_bns_rate.plot(scenario.bns_rate,np.exp(scenario.bns_rate_pdf), \
-                color='k', linestyle=linestyles[p], label=r'%s'%label_str)
+                color='k', linestyle=linestyles[e], label=r'%s'%label_str)
         ax_bns_rate.axvline(scenario.predicted_bns_rate * 1e6, color='r',\
                 label="`True' value")
 
@@ -80,29 +87,30 @@ for e,epoch in enumerate(epochs):
         ul_jet = grbeams_utils.alpha_ul(jetpos.theta, \
                 jetpos.jet_pdf_1D, alpha=0.99)
         if ul_jet>biggest_ul_jet: biggest_ul_jet = ul_jet
-        label_str='$R_{\mathrm{%s}}$'%prediction
 
         ax_jet_angle.plot(jetpos.theta,jetpos.jet_pdf_1D, \
-                color='k', linestyle=linestyles[p],
+                color='k', linestyle=linestyles[e], 
                 label=r'%s'%label_str)
 
-    ax_bns_rate.set_title('Epoch=%s'%str(epoch))
+    tit_str='Rate: $R_{\mathrm{%s}}$'%prediction
+    ax_bns_rate.set_title('%s'%str(tit_str))
     ax_bns_rate.legend()
     ax_bns_rate.set_xlabel('BNS Coalescence Rate $R$ [Mpc$^{-3}$ Myr$^{-1}$]')
     ax_bns_rate.set_ylabel('$p(R|N_{\mathrm{det}},T_{\mathrm{obs}},I)$')
     ax_bns_rate.minorticks_on()
     ax_bns_rate.grid(which='major',color='grey',linestyle='-')
 
-    ax_jet_angle.set_title('Epoch=%s'%str(epoch))
+    ax_jet_angle.set_title('%s'%str(tit_str))
     ax_jet_angle.legend()
     ax_jet_angle.set_xlabel(r'$\theta_{\mathrm{jet}}$')
     ax_jet_angle.set_ylabel(r'$p(\theta_{\mathrm{jet}}|R,I)$')
     ax_jet_angle.minorticks_on()
     ax_jet_angle.grid(which='major',color='grey',linestyle='-')
 
+    #pl.show()
     #sys.exit()
 
-pl.subplots_adjust(bottom=0.1,top=0.95)
+pl.subplots_adjust(bottom=0.1,top=0.925)
 pl.show()
 
 
