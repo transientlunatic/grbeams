9034c97653056e2978d724f4b7cdf98748828cf1
Daniel Williams <mail@daniel-williams.co.uk>
2016-03-28 18:50:37 +0100

    Updated requirements.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('README.rst') as readme_file:
     readme = readme_file.read()
 
 requirements = [
-    # TODO: put package requirements here
+    "numpy", "scipy", "astropy"
 ]
 
 test_requirements = [
