cc75257f5c36d0480212acdbb752b49233b95010
Daniel Williams <daniel.williams@ligo.org>
2017-12-13 14:03:03 +0000

    More tidying.

--- aLIGO/real_aligo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# Injection/testing script.  Produce results for known grb beaming angle.
-
-../grbeams_ADE.py delta,0.5 
-
-#../grbeams_ADE.py uniform 
-
-#../grbeams_ADE.py jeffreys 
